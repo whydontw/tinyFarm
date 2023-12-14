e00c8c2e0f7d611f806034a3895c401b9406939a
naranggggg <nara662046@gmail.com>
2023-12-14 17:14:17 +0900

    충돌 해결

--- tinyFarm/src/main/java/com/kh/tinyfarm/product/model/dao/ProductDao.java
+++ tinyFarm/src/main/java/com/kh/tinyfarm/product/model/dao/ProductDao.java
@@ -2,9 +2,11 @@ package com.kh.tinyfarm.product.model.dao;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.tinyfarm.common.model.vo.Attachment;
 import com.kh.tinyfarm.common.model.vo.PageInfo;
 import com.kh.tinyfarm.product.model.vo.Product;
 
@@ -20,7 +22,32 @@ public class ProductDao {
 	//상품 리스트
 	public ArrayList<Product> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
 		
-		return (ArrayList)sqlSession.selectList("productMapper.selectList");
+		//몇개씩 보여줄지
+		int limit = pi.getBoardLimit();
+		//몇개를 건너뛸지 
+		int offset = (pi.getCurrentPage() -1 ) * limit;
+		
+		RowBounds rowBounds = new RowBounds(offset,limit);
+		//rowbounds 객체를 전달해야하는데 전달할 파라미터(전달값)이 없다면 null을 넣어서 메소드 호출하기.
+		return (ArrayList)sqlSession.selectList("productMapper.selectList",null,rowBounds);
+	}
+
+	//상품 조회수 증가
+	public int increaseCount(SqlSessionTemplate sqlSession, int pno) {
+		
+		return sqlSession.selectOne("productMapper.increaseCount", pno);
+	}
+
+	//상품 조회
+	public Product selectProduct(SqlSessionTemplate sqlSession, int pno) {
+		
+		return sqlSession.selectOne("productMapper.selectProduct", pno);
+	}
+
+	//상품 등록
+	public int insertProduct(SqlSessionTemplate sqlSession, Product p, Attachment a) {
+
+		return sqlSession.insert("productMapper.insertProduct", p);
 	}
 
 }
